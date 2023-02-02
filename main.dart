46a169bd284e2b5b6b655111c5e9c0eba1edfed5
JuJcob <124314709+JuJcob@users.noreply.github.com>
2023-02-02 20:16:54 -0300

    main.dart

---
+++ main.dart
@@ -0,0 +1,53 @@
+void main() { 
+  // Exercicio 1 
+  print('\n Exercicio 1 :');
+  print('\n');
+ var pi = 3.14 ;
+ var raio = 4 ;
+  
+  var area = pi * raio * raio ;
+   
+print ('Area do circulo do raio $raio igual a : $area');
+ 
+// Exercicio 2 
+  
+print('\nExercicio 2 :');
+    print('\n');
+  
+  var chapa = 30;
+    var horas = 150;
+   var paghora = 7.90;
+  
+    var total = horas * paghora ;
+  
+  print ('salario do funcionario chapa $chapa igual à $total');
+  
+  // Exercicio 3
+  
+ print('''\n Exercicio 3 : '''); 
+    print('\n');
+      
+var nome = " bruno " ;
+ var sal = 600 ;
+ var vendas = 470;
+  var vendaporct = 70.5 ;
+  
+  var salariofinal = sal + vendaporct ;
+
+ print ("Bruno receberá o total de R\$ $salariofinal  ");
+  
+  
+  // Exercicio 4 
+  print("\n Exercicio 4 : ");
+  print("\n");
+    
+    var valorQ = 50;
+  var valorJ = 12 ;
+ var valorU = 100;
+ 
+  print("${valorJ + valorU / valorQ }");
+  
+  
+
+  
+}
