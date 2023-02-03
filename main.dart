30319e827bd080eb985aa12cb541ff28d8cd22dd
JuJcob <124314709+JuJcob@users.noreply.github.com>
2023-02-02 23:05:53 -0300

    Update main.dart
    exercícios da aula 1

--- main.dart
+++ main.dart
@@ -1,53 +1,57 @@
-void main() { 
-  // Exercicio 1 
+void main() {
+  // Exercicio 1
   print('\n Exercicio 1 :');
   print('\n');
- var pi = 3.14 ;
- var raio = 4 ;
-  
-  var area = pi * raio * raio ;
-   
-print ('Area do circulo do raio $raio igual a : $area');
- 
-// Exercicio 2 
-  
-print('\nExercicio 2 :');
-    print('\n');
-  
-  var chapa = 30;
-    var horas = 150;
-   var paghora = 7.90;
-  
-    var total = horas * paghora ;
-  
-  print ('salario do funcionario chapa $chapa igual à $total');
-  
-  // Exercicio 3
-  
- print('''\n Exercicio 3 : '''); 
-    print('\n');
-      
-var nome = " bruno " ;
- var sal = 600 ;
- var vendas = 470;
-  var vendaporct = 70.5 ;
-  
-  var salariofinal = sal + vendaporct ;
+  var pi = 3.14;
+  var raio = 4;
 
- print ("Bruno receberá o total de R\$ $salariofinal  ");
-  
-  
-  // Exercicio 4 
+  var area = pi * raio * raio;
+
+  print('Area do circulo do raio $raio igual a : $area');
+
+// Exercicio 2
+
+  print('\nExercicio 2 :');
+  print('\n');
+
+  var chapa = 30;
+  var horas = 150;
+  var paghora = 7.90;
+
+  var total = horas * paghora;
+
+  print('salario do funcionario chapa $chapa igual à $total');
+
+  // Exercicio 3
+
+  print('''\n Exercicio 3 : ''');
+  print('\n');
+
+  var nome = " bruno ";
+  var sal = 600;
+  var vendas = 470;
+  //15% de vendas
+  var vendaporct = 70.5;
+
+  var salariofinal = sal + vendaporct;
+
+  print("Bruno receberá o total de R\$ $salariofinal  ");
+
+  // Exercicio 4
   print("\n Exercicio 4 : ");
   print("\n");
-    
-    var valorQ = 50;
-  var valorJ = 12 ;
- var valorU = 100;
- 
-  print("${valorJ + valorU / valorQ }");
-  
-  
 
-  
+  var valorQ = 50;
+  var valorJ = -12;
+  var valorU = 100;
+
+  print("resultado: ${valorJ + valorU + valorQ}");
+
+  // Exercicio 5
+  print("\n Exercicio 5 : ");
+  print("\n");
+
+  var valor1 = 12000 / 23000;
+  var valor2 = 100;
+  print('resultado: ${valor1 * valor2}% ');
 }
